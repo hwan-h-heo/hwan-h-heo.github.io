2d2268df5142d941c9112cd1af51a38184dd3e4d
hwanheo <gjghks950@naver.com>
2024-09-17 18:01:48 +0900

    add 3dgs-js

---
+++ server.py
@@ -0,0 +1,10 @@
+import http.server
+
+class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
+        return super(CORSRequestHandler, self).end_headers()
+
+if __name__ == '__main__':
+    http.server.test(HandlerClass=CORSRequestHandler)
