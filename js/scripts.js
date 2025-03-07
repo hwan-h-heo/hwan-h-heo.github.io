bfb856cfd6fddfbaa9e28c0d5a7bcd3ab4e2b818
hwanheo <gjghks950@naver.com>
2025-03-07 21:38:18 +0900

    add img lazy loading

--- js/scripts.js
+++ js/scripts.js
@@ -131,3 +131,21 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+let scrollTop = document.querySelector('.scroll-top');
+
+function toggleScrollTop() {
+    if (scrollTop) {
+        window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+}
+scrollTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+window.addEventListener('load', toggleScrollTop);
+document.addEventListener('scroll', toggleScrollTop);
