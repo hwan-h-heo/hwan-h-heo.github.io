bdd7bebfeabcb136e9d7af6c42e3fdd35d191952
hwanheo <gjghks950@naver.com>
2025-08-05 01:17:26 +0900

    update home

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import http.server
 
-class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
+class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header("Cross-Origin-Opener-Policy", "same-origin")
-        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
-        return super(CORSRequestHandler, self).end_headers()
+        # COEP 정책을 credentialless로 완화
+        self.send_header("Cross-Origin-Embedder-Policy", "credentialless")
+        super().end_headers()
 
 if __name__ == '__main__':
-    http.server.test(HandlerClass=CORSRequestHandler, port=8013)
+    http.server.test(HandlerClass=MyRequestHandler, port=8013)
